da85012129b6af7788242d9a2afb513922a71123
Kristan <itan_07@hotmail.com>
2025-06-11 22:28:11 -0700

    Added more tests

---
+++ TecApiWrapperTesting/Helpers/HtmlPageHelper.Tests.cs
@@ -0,0 +1,31 @@
+﻿using AngleSharp.Dom;
+using TecWrapperApi.Helpers;
+namespace TecApiWrapperTesting.Helpers;
+
+
+public class HtmlPageHelperTests
+{
+    private readonly string LoginPage = MockPages.LoginPage;
+    private readonly string MainPage = MockPages.MainPage;
+    
+    [Fact]
+    public void IsLoginPage_MustReturnTrueWithLoginPage()
+    {
+        bool found = HtmlPageHelper.IsLoginPage(this.LoginPage);
+        Assert.True(found);
+    }
+
+    [Fact]
+    public void IsLoginPage_MustReturnFalseIfNotLoginPage()
+    {
+        bool found = HtmlPageHelper.IsLoginPage(this.MainPage);
+        Assert.False(found);
+    }
+
+    [Theory]
+    [InlineData("asdf", "asdf", " asdf" )]
+    public void FindFirst_MustFindCorrectlyHtmlElement(string htmlContent, string cssSelector, string expectedElementType)
+    {
+        
+    }
+}
